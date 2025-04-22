21cc5ac6b6b12c3862c028a2f250be571fac7b63
Gabriel Cumara <gabrielcumara@gmail.com>
2025-04-22 18:53:19 -0400

    commit rama uno_1

--- src/main/java/com/example/proy2/controller/HomeController.java
+++ src/main/java/com/example/proy2/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String saludo(){
-        return "Hello Gummar...";
+        return "Hola desde la rama uno";
     }
 }
